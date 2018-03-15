27dbebf5dd4c3015be2ecf40143852397468f79a
danieltheo90 <dtheo90@yahoo.com>
2018-03-15 16:39:55 +0700

    second commit 2

--- server/server.js
+++ server/server.js
@@ -5,29 +5,12 @@ const socketIO = require('socket.io');
 
 
 const publicPath = path.join(__dirname,'../public');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 
 app.use(express.static(publicPath));
-
-io.on('connection',(socket)=>{
-	console.log('New User connected');
-	
-
-	socket.on('createMessage',(message)=>{
-		console.log('createMessage',message);
-		io.emit('newMessage',{
-			from: message.from,
-			text: message.text,
-			createdAt: new Date().getTime()
-		});
-	});
-	socket.on('disconnect', () => {
-    	console.log('User was disconnected');
-    });
-})
 
 server.listen(port, ()=>{
 	console.log('Server is up port '+ port);
